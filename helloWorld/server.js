8d792362945d470702510610546621be79e21d4a
Cristina <cristina.j.smith@asu.edu>
2017-01-30 16:42:52 -0700

    server change

--- helloWorld/server.js
+++ helloWorld/server.js
@@ -6,9 +6,5 @@ function callback (req, res) { // req -> request object; res -> response object
 }
 
 var server = http.createServer(callback) // create an http server
-server.listen(8080, "
-34.198.35.134
-"); // make server listen to port 1234
-console.log("Server running at: "+ "http://
-34.198.35.134
-:8080");
+server.listen(8080, "34.198.35.134"); // make server listen to port 1234
+console.log("Server running at: "+ "http://34.198.35.134:8080");
