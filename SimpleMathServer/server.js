afed6c0887d94c9e74da0b01c0818b6b36fdc651
Cristina <cristina.j.smith@asu.edu>
2017-02-03 16:45:29 -0700

    more callbacks added

--- SimpleMathServer/server.js
+++ SimpleMathServer/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8081;
+var port = 8080;
 
 app.get("/", function (req, res) {
       res.redirect("/index.html");
@@ -12,14 +12,40 @@ app.get("/", function (req, res) {
 
 
 var addCallback = function (req, res) {
-    console.log(req.query);
-	var a = parseFloat(req.query.a);
-	var b = parseFloat(req.query.b);
-    var result = a + b;
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result  = a + b;
 
     res.send(result.toString()); // send response body
 }
+
+var subCallback = function (req, res) {
+  var a = parseFloat(req.query.a);
+  var b = parseFloat(req.query.b);
+  var result = a - b;
+
+  res.send(result.toString());
+}
+
+var multCallback = function (req, res) {
+  var a = parseFloat(req.query.a);
+  var b = parseFloat(req.query.b);
+  var result = a * b;
+
+  res.send(result.toString());
+}
+var divCallback = function (req, res) {
+  var a = parseFloat(req.query.a);
+  var b = parseFloat(req.query.b);
+  var result = a / b;
+
+  res.send(result.toString());
+}
+
 app.get("/add", addCallback);
+app.get("/sub", subCallback);
+app.get("/mult", multCallback);
+app.get("/div", divCallback);
 
 app.use(methodOverride());
 app.use(bodyParser());
