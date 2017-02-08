7fafb971c71508779935daacb2b3981a1a95e7c4
student <student@dc-62.mobile.asu.edu>
2017-02-08 09:44:58 -0700

    server edit

--- ToDoExample/server.js
+++ ToDoExample/server.js
@@ -10,42 +10,14 @@ app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
+var todoList = [];
 
 var addCallback = function (req, res) {
 	console.log(req.query);
-    var a = parseFloat(req.query.a);
-    var b = parseFloat(req.query.b);
-    var result  = a + b;
+	todoList.push(req.query);
 
-    res.send(result.toString()); // send response body
+    res.send('1'); 
 }
-
-var subCallback = function (req, res) {
-  var a = parseFloat(req.query.a);
-  var b = parseFloat(req.query.b);
-  var result = a - b;
-
-  res.send(result.toString());
-}
-
-var multCallback = function (req, res) {
-  var a = parseFloat(req.query.a);
-  var b = parseFloat(req.query.b);
-  var result = a * b;
-
-  res.send(result.toString());
-}
-var divCallback = function (req, res) {
-  var a = parseFloat(req.query.a);
-  var b = parseFloat(req.query.b);
-  var result = a / b;
-
-  res.send(result.toString());
-}
-
-app.get("/sub", subCallback);
-app.get("/mult", multCallback);
-app.get("/div", divCallback);
 
 app.get("/addTodo", addCallback);
 
