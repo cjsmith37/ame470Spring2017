b68c04499f04f454d5f592eb02852713601f0d9c
student <student@dc-76.dhcp.asu.edu>
2017-03-13 09:32:32 -0700

    files added to Assignment4

--- Assignment4/server.js
+++ Assignment4/server.js
@@ -1,13 +1,14 @@
 var express = require("express");
-var app = express();
+var server = express();
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
 
+var db = require('mongoskin').db('mongodb://user:pwd@127.0.0.1:27017/tododb');
 
-app.get("/", function (req, res) {
+server.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
@@ -16,28 +17,53 @@ var todoList = [];
 
 
 
-app.get("/addTodo", function (req, res) {
-   todoList.push(req.query);
-   res.send(JSON.stringify(todoList));
+server.get("/addTodo", function (req, res) {
+  db.collection("data").insert(req.query, function(err, result){
+      if(err){
+        res.send("error"); 
+      }
+      else{
+        db.collection("data").find({}).toArray( function(err1, result1) {
+          res.send(JSON.stringify(result1));
+        });
+      }
+  });
+   // todoList.push(req.query);
+   // res.send(JSON.stringify(todoList));
 });
 
 
-app.get("/deleteTodo", function (req, res) {
+server.get("/deleteTodo", function (req, res) {
    //var id = parseInt(req.query.id);
    var id = req.query.id.toString();
    console.log(id);
-   res.send(JSON.stringify(todoList));
-   todoList.splice(index,1);
+   db.collection("data").remove({id: id}, function(err, result){
+     console.log(err);
+      if(err){
+        res.send("error"); 
+      }
+      else{
+        db.collection("data").find({}).toArray( function(err1, result1) {
+          res.send(JSON.stringify(result1));
+        });
+      }
+   });
+   // res.send(JSON.stringify(todoList));
+   // todoList.splice(index,1);
 });
 
-app.get("/getTodos", function (req, res) {
-	res.send(JSON.stringify(todoList));
+server.get("/getTodos", function (req, res) {
+  db.collection("data").find({}).toArray( function(err, result) {
+    res.send(JSON.stringify(result));
+  });
+
+   // res.send(JSON.stringify(todoList));
 });
 
-app.use(methodOverride());
-app.use(bodyParser());
-app.use(express.static(__dirname + '/public'));
-app.use(errorHandler());
+server.use(methodOverride());
+server.use(bodyParser());
+server.use(express.static(__dirname + '/public'));
+server.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
-app.listen(port);
+server.listen(port);
