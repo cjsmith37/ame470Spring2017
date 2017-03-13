1c60eae51d105e6b476ca0c7609bb253f43009d1
student <student@dc-76.dhcp.asu.edu>
2017-03-13 09:13:15 -0700

    assignment 4 added

---
+++ Assignment4/server.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+
+
+app.get("/", function (req, res) {
+      res.redirect("/index.html");
+});
+
+
+var todoList = [];
+
+
+
+app.get("/addTodo", function (req, res) {
+   todoList.push(req.query);
+   res.send(JSON.stringify(todoList));
+});
+
+
+app.get("/deleteTodo", function (req, res) {
+   //var id = parseInt(req.query.id);
+   var id = req.query.id.toString();
+   console.log(id);
+   res.send(JSON.stringify(todoList));
+   todoList.splice(index,1);
+});
+
+app.get("/getTodos", function (req, res) {
+	res.send(JSON.stringify(todoList));
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
