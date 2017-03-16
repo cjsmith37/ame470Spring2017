811e192ff878d457bfacb57c2335a6578614cccf
Cristina S <cristina.j.smith@asu.edu>
2017-03-16 13:13:39 -0700

    Update server.js

--- Assignment4/server.js
+++ Assignment4/server.js
@@ -4,9 +4,9 @@ var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8083;
+var port = 8080;
 
-var db = require('mongoskin').db('mongodb://user:pwd@127.0.0.1:28017/picturedb');
+var db = require('mongoskin').db('mongodb://user:pwd@127.0.0.1:27017/picturedb');
 
 server.get("/", function (req, res) {
       res.redirect("/index.html");
