381735aa64cfea3646f6e958a117bce5de71e11f
Cristina <cristina.j.smith@asu.edu>
2017-04-05 00:57:41 -0700

    edit

--- Assignment4/server.js
+++ Assignment4/server.js
@@ -49,10 +49,20 @@ server.get("/deletePic", function (req, res) {
 
 server.get("/renameImage", function (req, res) {
   var id = req.query.id.toString();
+  console.log(id);
   db.collection("data").findOne({id:id}, function(err, result) {
-    res.send(JSON.stringify(result));
+    console.log(err);
+     if(err){
+       res.send("error");
+     }
+     else{
+       db.collection("data").find({}).toArray( function(err1, result1) {
+         res.send(JSON.stringify(result1));
+       });
+     }
   });
 });
+
 
 
 server.get("/getPics", function (req, res) {
