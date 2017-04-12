f144aeb257637609afa47448575d0bd14496b8e5
student <student@dc-63.mobile.asu.edu>
2017-04-12 09:25:19 -0700

    add files

--- Assignment6/server.js
+++ Assignment6/server.js
@@ -1,68 +1,57 @@
+var fs = require('fs');
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./credentials.json');
+var s3 = new AWS.S3();
+
 var express = require("express");
-var server = express();
+var app = express();
 var bodyParser = require('body-parser');
+
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8084;
+var port = 8080;
+app.use(methodOverride());
+//app.use(bodyParser());
+app.use(require('connect').bodyParser());
 
-var db = require('mongoskin').db('mongodb://user:pwd@127.0.0.1:27017/picturedb');
 
-server.get("/", function (req, res) {
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
-
-var picList = [];
-
-server.get("/addPic", function (req, res) {
-  db.collection("data").insert(req.query, function(err, result){
-      if(err){
-        res.send("error");
-      }
-      else{
-        db.collection("data").find({}).toArray( function(err1, result1) {
-          res.send(JSON.stringify(result1));
+app.post('/uploadFile', function(req, res){
+      console.log(req.body);
+        var intname = req.body.fileInput;
+        var filename = req.files.input.name;
+        var fileType =  req.files.input.type;
+        var tmpPath = req.files.input.path;
+        var s3Path = '/' + intname;
+        
+        fs.readFile(tmpPath, function (err, data) {
+            var params = {
+                Bucket:'ame470s2017tg',
+                ACL:'public-read',
+                Key:intname,
+                Body: data,
+                ServerSideEncryption : 'AES256'
+            };
+            s3.putObject(params, function(err, data) {
+                console.log(err);
+                res.end("success");
+            });
         });
-      }
   });
-});
 
-
-server.get("/deletePic", function (req, res) {
-   //var id = parseInt(req.query.id);
-   var id = req.query.id.toString();
-   console.log(id);
-   db.collection("data").remove({id: id}, function(err, result){
-     console.log(err);
-      if(err){
-        res.send("error");
-      }
-      else{
-        db.collection("data").find({}).toArray( function(err1, result1) {
-          res.send(JSON.stringify(result1));
-        });
-      }
-   });
-});
-
-server.get("/getPics", function (req, res) {
-  db.collection("data").find({}).toArray( function(err, result) {
-    res.send(JSON.stringify(result));
-  });
-});
-
-server.get("/getPic", function (req, res) {
-  var id = req.query.id.toString();
-  db.collection("data").findOne({id:id}, function(err, result) {
-    res.send(JSON.stringify(result));
-  });
-});
-
-server.use(methodOverride());
-server.use(bodyParser());
-server.use(express.static(__dirname + '/public'));
-server.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
-server.listen(port);
+app.listen(port);
