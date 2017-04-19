6e2d8c33514f80084d130e47ccec9829d1fe5df4
student <student@dc-67.mobile.asu.edu>
2017-04-19 09:34:00 -0700

    edit server

--- Assignment6/server.js
+++ Assignment6/server.js
@@ -6,55 +6,68 @@ var s3 = new AWS.S3();
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8089;
+app.use(methodOverride());
+//app.use(bodyParser());
+app.use(require('connect').bodyParser());
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
 
 app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
 app.post('/uploadImage', function(req, res){
-        var intname = req.body.fileInput;
-        var s3Path = '/' + intname;
-        var buf = new Buffer(req.body.data.replace(/^data:image\/\w+;base64,/, ""),'base64');
+    var intname = req.body.fileInput;
+    var s3Path = '/' + intname;
+    var buf = new Buffer(req.body.data.replace(/^data:image\/\w+;base64,/, ""),'base64');
+    var params = {
+        Bucket:'ame470spring2017',
+        ACL:'public-read',
+        Key:intname,
+        Body: buf,
+        ServerSideEncryption : 'AES256'
+    };
+    s3.putObject(params, function(err, data) {
+        console.log(err);
+        res.end("success");
+    });
+});
+
+app.post('/uploadFile', function(req, res){
+    var intname = req.body.fileInput;
+    var filename = req.files.input.name;
+    var fileType =  req.files.input.type;
+    var tmpPath = req.files.input.path;
+    var s3Path = '/' + intname;
+    
+    fs.readFile(tmpPath, function (err, data) {
         var params = {
             Bucket:'ame470spring2017',
             ACL:'public-read',
             Key:intname,
-            Body: buf,
+            Body: data,
             ServerSideEncryption : 'AES256'
         };
         s3.putObject(params, function(err, data) {
-            console.log(err);
             res.end("success");
+            console.log(err);
         });
+    });
   });
 
-
-app.post('/uploadFile', function(req, res){
-      console.log(req.body);
-        var intname = req.body.fileInput;
-        var filename = req.files.input.name;
-        var fileType =  req.files.input.type;
-        var tmpPath = req.files.input.path;
-        var s3Path = '/' + intname;
-
-        fs.readFile(tmpPath, function (err, data) {
-            var params = {
-                Bucket:'ame470spring2017',
-                ACL:'public-read',
-                Key:intname,
-                Body: data,
-                ServerSideEncryption : 'AES256'
-            };
-            s3.putObject(params, function(err, data) {
-                res.end("success");
-                console.log(err);
-            });
-        });
-  });
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
 app.listen(port);
