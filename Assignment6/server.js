40e8129a40b526c765359d44b69f6d36b4ce80b6
Cristina <cristina.j.smith@asu.edu>
2017-04-18 23:38:47 -0700

    edit

--- Assignment6/server.js
+++ Assignment6/server.js
@@ -6,15 +6,13 @@ var s3 = new AWS.S3();
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
-
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8080;
+var port = 8086;
 app.use(methodOverride());
 //app.use(bodyParser());
 app.use(require('connect').bodyParser());
-
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -25,11 +23,12 @@ app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
 app.use(errorHandler());
 
-app.get("/", function (req, res) {
+
+server.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
-  app.post('/uploadImage', function(req, res){
+server.post('/uploadImage', function(req, res){
         var intname = req.body.fileInput;
         var s3Path = '/' + intname;
         var buf = new Buffer(req.body.data.replace(/^data:image\/\w+;base64,/, ""),'base64');
@@ -47,14 +46,14 @@ app.get("/", function (req, res) {
   });
 
 
-app.post('/uploadFile', function(req, res){
+server.post('/uploadFile', function(req, res){
       console.log(req.body);
         var intname = req.body.fileInput;
         var filename = req.files.input.name;
         var fileType =  req.files.input.type;
         var tmpPath = req.files.input.path;
         var s3Path = '/' + intname;
-        
+
         fs.readFile(tmpPath, function (err, data) {
             var params = {
                 Bucket:'ame470spring2017',
@@ -70,6 +69,5 @@ app.post('/uploadFile', function(req, res){
         });
   });
 
-
 console.log("Simple static server listening at http://" + hostname + ":" + port);
-app.listen(port);
+server.listen(port);
