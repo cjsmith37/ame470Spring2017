0546fbbb9f8d3033a11b568b1b3d87cf534e8623
student <student@dc-67.mobile.asu.edu>
2017-04-19 10:17:17 -0700

    edit

--- Assignment6/server.js
+++ Assignment6/server.js
@@ -13,8 +13,8 @@ var port = 8089;
 
 
 app.use(methodOverride());
-app.use(bodyParser());
-//app.use(require('connect').bodyParser());
+//app.use(bodyParser());
+app.use(require('connect').bodyParser());
 
 
 // parse application/x-www-form-urlencoded
