b2b2f32b75ed1f18061f1634a27ca581b558ae86
student <student@dc-62.mobile.asu.edu>
2017-04-24 10:06:00 -0700

    edit

--- Assignment6/server.js
+++ Assignment6/server.js
@@ -16,11 +16,13 @@ app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 app.use(errorHandler());
 
+var db = require('mongoskin').db('mongodb://user:pwd@127.0.0.1:27017/picturedb');
+
 app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
-
+var pictureList = [];
 
 app.post('/uploadImage', function(req, res){
     var intname = req.body.fileInput;
@@ -60,6 +62,54 @@ app.post('/uploadFile', function(req, res){
         });
     });
   });
+  
+  server.get("/addPic", function (req, res) {
+    db.collection("data").insert(req.query, function(err, result){
+        if(err){
+          res.send("error"); 
+        }
+        else{
+          db.collection("data").find({}).toArray( function(err1, result1) {
+            res.send(JSON.stringify(result1));
+          });
+        }
+    });
+  });
+  
+  server.get("/deletePic", function (req, res) {
+     var id = req.query.id.toString();
+     console.log(id);
+     db.collection("data").remove({id: id}, function(err, result){
+       console.log(err);
+        if(err){
+          res.send("error"); 
+        }
+        else{
+          db.collection("data").find({}).toArray( function(err1, result1) {
+            res.send(JSON.stringify(result1));
+          });
+        }
+     });
+  });
+  
+  server.get("/getPics", function (req, res) {
+    db.collection("data").find({}).toArray( function(err, result) {
+      res.send(JSON.stringify(result));
+    });
+  });
 
+  server.get("/getPic", function (req, res) {
+    var id = req.query.id.toString();
+    db.collection("data").findOne({id:id}, function(err, result) {
+      res.send(JSON.stringify(result));
+    });
+  });
+  
+server.use(methodOverride());
+server.use(bodyParser());
+server.use(express.static(__dirname + '/public'));
+server.use(errorHandler());
+  
 console.log("Simple static server listening at http://" + hostname + ":" + port);
 app.listen(port);
+server.listen(port);
