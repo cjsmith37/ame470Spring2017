180683c3854cfc1dfaec671b66c5722cc8e95178
Cristina <cristina.j.smith@asu.edu>
2017-04-25 21:09:32 -0700

    edit assignment 6

--- Assignment6/server.js
+++ Assignment6/server.js
@@ -9,18 +9,25 @@ var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8089;
+var port = 8080;
 
 app.use(methodOverride());
-app.use(bodyParser());
+//app.use(bodyParser());
+app.use(require('connect').bodyParser());
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.use(express.static(__dirname + '/public'));
 app.use(errorHandler());
 
 app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
-
-
 
 app.post('/uploadImage', function(req, res){
     var intname = req.body.fileInput;
