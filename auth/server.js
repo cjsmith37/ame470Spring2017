3747db50d7cfcaeda2e5adadf8a12160eb66b6f8
student <student@dc-study-1.dhcp.asu.edu>
2017-05-03 07:38:33 -0700

    edit

--- auth/server.js
+++ auth/server.js
@@ -4,7 +4,7 @@ var express = require("express"),
 	errorHandler = require('errorhandler'),
 	methodOverride = require('method-override'),
 	hostname = process.env.HOSTNAME || 'localhost',
-	port = 8088;
+	port = 8089;
 	
 app.get("/", function (req, res) {
 	  res.redirect("/index.html");
