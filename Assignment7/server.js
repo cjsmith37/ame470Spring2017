602eed35d31c9ed82e46b68a15b34d746228b5ac
Cristina <cristina.j.smith@asu.edu>
2017-05-02 21:00:20 -0700

    edit

--- Assignment7/server.js
+++ Assignment7/server.js
@@ -4,109 +4,36 @@ AWS.config.loadFromPath('./credentials.json');
 var s3 = new AWS.S3();
 
 var express = require("express");
-var server = express();
+var app = express();
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
 
-server.use(methodOverride());
+app.use(methodOverride());
 //app.use(bodyParser());
-server.use(require('connect').bodyParser());
+app.use(require('connect').bodyParser());
+
+
 
 // parse application/x-www-form-urlencoded
-server.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-server.use(bodyParser.json())
+app.use(bodyParser.json())
 
-server.use(express.static(__dirname + '/public'));
-server.use(errorHandler());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
 
-var db = require('mongoskin').db('mongodb://user:pwd@127.0.0.1:27017/tododb');
 
-server.get("/", function (req, res) {
+app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
 
-var todoList = [];
 
-server.get("/addTodo", function (req, res) {
-  db.collection("data").insert(req.query, function(err, result){
-      if(err){
-        res.send("error");
-      }
-      else{
-        db.collection("data").find({}).toArray( function(err1, result1) {
-          res.send(JSON.stringify(result1));
-        });
-      }
-  });
-});
-
-
-server.get("/renameTodo", function (req, res) {
-   var id = req.query.id.toString();
-   console.log(id);
-  db.collection("data").findOne({id:id}, function(err, result) {
-    if(result){
-      result.name = req.query.name;
-      db.collection("data").save(result, function(e){
-        db.collection("data").find({}).toArray( function(err1, result1) {
-          res.send(JSON.stringify(result1));
-        });
-      });
-    }
-  });
-   db.collection("data").remove({id: id}, function(err, result){
-     console.log(err);
-      if(err){
-        res.send("error");
-      }
-      else{
-        db.collection("data").find({}).toArray( function(err1, result1) {
-          res.send(JSON.stringify(result1));
-        });
-      }
-   });
-});
-
-
-server.get("/deleteTodo", function (req, res) {
-   //var id = parseInt(req.query.id);
-   var id = req.query.id.toString();
-   console.log(id);
-   db.collection("data").remove({id: id}, function(err, result){
-     console.log(err);
-      if(err){
-        res.send("error");
-      }
-      else{
-        db.collection("data").find({}).toArray( function(err1, result1) {
-          res.send(JSON.stringify(result1));
-        });
-      }
-   });
-});
-
-server.get("/getTodos", function (req, res) {
-  db.collection("data").find({}).toArray( function(err, result) {
-    res.send(JSON.stringify(result));
-  });
-});
-
-server.get("/getTodo", function (req, res) {
-  var id = req.query.id.toString();
-  db.collection("data").findOne({id:id}, function(err, result) {
-    res.send(JSON.stringify(result));
-  });
-});
-
-
-
-server.post('/uploadImage', function(req, res){
+app.post('/uploadImage', function(req, res){
     var intname = req.body.fileInput;
     var s3Path = '/' + intname;
     var buf = new Buffer(req.body.data.replace(/^data:image\/\w+;base64,/, ""),'base64');
@@ -123,7 +50,7 @@ server.post('/uploadImage', function(req, res){
     });
 });
 
-server.post('/uploadFile', function(req, res){
+app.post('/uploadFile', function(req, res){
     var intname = req.body.fileInput;
     var filename = req.files.input.name;
     var fileType =  req.files.input.type;
@@ -146,4 +73,4 @@ server.post('/uploadFile', function(req, res){
   });
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
-server.listen(port);
+app.listen(port);
